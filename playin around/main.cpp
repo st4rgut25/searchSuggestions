aa423be0baca99463c3c96578da1f8127cd2fba7
edward.arbor@gmail.com <edward.arbor@gmail.com>
2017-05-25 18:15:50 -0700

    Initial Commit

---
+++ playin around/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  playin around
+//
+//  Created by Ed cox on 5/25/17.
+//  Copyright © 2017 Ed cox. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
